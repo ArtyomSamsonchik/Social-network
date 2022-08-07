ee64c6662aa4441a3ee36b153d9d6d337d24f8e4
ArtyomSamsonchik <colpachek@gmail.com>
2022-08-07 22:47:52 +0300

    Add basic page structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className="app">
+        <div className={"header"}>Header</div>
+        <aside className={"navbar"}>
+            <nav>
+                <ul>
+                    <li>Мой профиль</li>
+                    <li>Сообщения</li>
+                    <li>Новости</li>
+                    <li>Музыка</li>
+                    <li>Фотографии</li>
+                    <li>Друзья</li>
+                </ul>
+            </nav></aside>
+        <div className={"main-content"}>Main</div>
+        <div className={"footer"}>Footer</div>
     </div>
   );
 }
