b52d572ff3ed52e31ad02e08e8674c82e2d4ce43
ArtyomSamsonchik <colpachek@gmail.com>
2022-11-09 18:06:33 +0300

    Quick props fix.

--- src/Components/Header/HeaderContainer.tsx
+++ src/Components/Header/HeaderContainer.tsx
@@ -4,7 +4,6 @@ import {Header} from "./Header";
 
 type HeaderContainerProps = {
     authData: AuthDataType
-    setUserAuthData: (loginData: Omit<AuthDataType, "loggedIn">) => void
     authorize: () => void
 }
 
